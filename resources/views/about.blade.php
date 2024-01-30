b3b5baf62f24d82f03207f1d88728c26da9ecc7f
RA MEY <129702894+RAMEY0406@users.noreply.github.com>
2024-01-30 16:11:29 +0700

    create route controller and view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Start laravel</title>
+    <title>About page</title>
 </head>
 <body>
-    <h1>Learn laravel</h1>
-    <h1>Learn laravel</h1>
+    <h1>About Page</h1>
+    <hr>
+    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, voluptatibus.</h1>
 </body>
 </html>
